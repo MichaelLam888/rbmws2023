37b8d1441425335b7d25d81e4ed476242d4b2d0e
gdmaskhk <91582476+gdmaskhk@users.noreply.github.com>
2023-02-27 18:33:11 +0800

    Add files via upload

--- src/Template_9.js
+++ src/Template_9.js
@@ -14,6 +14,7 @@ export default function Template_9() {
     <div>
       <p>感謝!</p>
      <p>Loan No. 貸款編號&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; :&nbsp; {cust_detial.loan_no} 已完成簽約程序,我們會盡快聯絡閣下</p>
+     <p>放款時間於簽約後下一個工作天13:00前，並安排存入指定銀行戶口。 還款資料會在還款期前一個星期以信息發給各下。敬請留意。</p>
     </div>
   )
 }
